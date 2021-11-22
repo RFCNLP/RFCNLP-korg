5b9fd421195f9f425ec9ba372c275d5ab3405f0e
anonymous-sp-submission <anonymous.sp.submission@gmail.com>
2021-11-22 11:37:54 -0500

    More useful comments in the code

--- korg/CLI.py
+++ korg/CLI.py
@@ -152,6 +152,13 @@ def handleTs(args, name):
 		return [addTrailNumberToArgs(args, i) for i in range(0, num)]
 
 def trailParseCMDs(tmpName):
+	"""
+	-t means "inspect a trail"
+	-s means "show all send events"
+	-r means "show all receive events"
+	-p means "show all state changes at each time step"
+	-g means "show global variable values at each time step"
+	"""
 	args = "spin -t -s -r -p -g " + tmpName
 	return handleTs(args, tmpName)
 
